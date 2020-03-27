b476058bba05fde1aa2db813df1f7ffc35b2dd21
Mohamed Samir <gm.mohamedsamir@gmail.com>
2020-03-27 03:37:14 +0200

    init

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 512);
+            this.ControlBox = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.LocationChanged += new System.EventHandler(this.Form1_LocationChanged);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.Move += new System.EventHandler(this.Form1_Move);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int TogMove;
+        int ValX;
+        int ValY;
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            TogMove = 1;
+            ValX = e.X;
+            ValY = e.Y;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            TogMove = 0;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (TogMove == 1)
+            {
+                this.SetDesktopLocation(MousePosition.X - ValX, MousePosition.Y - ValY);
+                
+            }
+            
+        }
+        bool b;
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            if (b)
+            {
+                this.Opacity = 50;
+                b = !b;
+            }
+            else
+            {
+                this.Opacity = 100;
+                b = !b;
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            if (b)
+            {
+                this.Opacity = 50;
+                b = !b;
+            }
+            else
+            {
+                this.Opacity = 100;
+                b = !b;
+            }
+        }
+
+        private void Form1_LocationChanged(object sender, EventArgs e)
+        {
+
+            if (b)
+            {
+                this.Opacity = 50;
+                b = !b;
+            }
+            else
+            {
+                this.Opacity = 100;
+                b = !b;
+            }
+        }
+
+        private void Form1_Move(object sender, EventArgs e)
+        {
+
+            if (b)
+            {
+                this.Opacity = 50;
+                b = !b;
+            }
+            else
+            {
+                this.Opacity = 100;
+                b = !b;
+            }
+        }
+    }
+}
